a9b70e8df9d61636f6e6ea6951d60aea9355408f
georgeduard <george@ganzdigital.com>
2025-09-20 20:34:51 +0300

    Fix typescript error

--- test/__mocks__/nextImageMock.tsx
+++ test/__mocks__/nextImageMock.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
 // Simple Next.js Image mock that renders a native img
-const MockedImage = React.forwardRef<HTMLImageElement, any>(function Image(
-  { src, alt, ...props },
-  ref,
+type MockImageProps = React.ComponentProps<'img'> & { src: string | { src: string } };
+
+const MockedImage = React.forwardRef<HTMLImageElement, MockImageProps>(function Image(
+  { src, alt, ...props }: MockImageProps,
+  ref: React.ForwardedRef<HTMLImageElement>,
 ) {
   // Next/Image can pass objects; ensure src is string for tests
   const url = typeof src === 'string' ? src : (src?.src ?? '');
